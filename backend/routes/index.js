cd107b43e604bf0ca156bed5922f5e758c705c4d
NupurPrakash <prakashnupurprakash@gmail.com>
2021-01-19 14:17:24 -0500

    module exported for index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-module.exports = db => {
+
+module.exports = (db) => {
   /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+return router;
 }
 
 
